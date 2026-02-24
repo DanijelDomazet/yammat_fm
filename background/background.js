22ca0e483981629eb7e3e3759e71ff7cda738dd8
Danijel Domazet <danijel.domazet@eu.walaris.com>
2026-02-24 10:54:55 +0100

    change icon when playing

--- background/background.js
+++ background/background.js
@@ -1,5 +1,32 @@
 "use strict";
 
+// Offscreen documents can't reliably access UI APIs like chrome.action,
+// so they ask the service worker to update the toolbar icon.
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message && message.action === "set_icon") {
+        const playing = message.state === "playing";
+
+        // chrome.action.setIcon expects resource paths relative to the extension root.
+        // Use absolute-extension paths to avoid "Failed to fetch" in some contexts.
+        const path = playing ? {
+            16: chrome.runtime.getURL("img/icons/ym16x16_play.png"),
+            32: chrome.runtime.getURL("img/icons/ym32x32_play.png"),
+            58: chrome.runtime.getURL("img/icons/ym58x58_play.png"),
+            120: chrome.runtime.getURL("img/icons/ym120x120_play.png"),
+        } : {
+            16: chrome.runtime.getURL("img/icons/ym16x16.png"),
+            32: chrome.runtime.getURL("img/icons/ym32x32.png"),
+            58: chrome.runtime.getURL("img/icons/ym58x58.png"),
+            120: chrome.runtime.getURL("img/icons/ym120x120.png"),
+        };
+
+        // Don't block; this is purely cosmetic.
+        chrome.action.setIcon({ path }).catch((err) => {
+            console.warn("setIcon failed:", err);
+        });
+    }
+});
+
  (async function initialize() {
      try {
          await createOffscreen();
--- offscreen/offscreen.js
+++ offscreen/offscreen.js
@@ -2,6 +2,15 @@ var audio = null;
 var isPlaying = false;
 var tryingToPlay = false;
 
+async function setActionIcon(state) {
+  try {
+    // Request the service worker to update the toolbar icon.
+    await chrome.runtime.sendMessage({ action: "set_icon", state });
+  } catch (e) {
+    console.warn("Failed to request action icon change:", e);
+  }
+}
+
 
 chrome.runtime.onMessage.addListener((message, sender, callback) => {
   if (message.action === "play") {
@@ -19,6 +28,7 @@ chrome.runtime.onMessage.addListener((message, sender, callback) => {
       console.info("Playing!");
       isPlaying = true;
       tryingToPlay = false;
+      setActionIcon("playing");
       callback(true);
     }).catch((err) => {
       tryingToPlay = false;
@@ -29,6 +39,8 @@ chrome.runtime.onMessage.addListener((message, sender, callback) => {
     console.info("pause");
     tryingToPlay = false;
     isPlaying = false;
+
+    setActionIcon("paused");
 
     if (audio) {
       audio.pause();
@@ -42,4 +54,23 @@ chrome.runtime.onMessage.addListener((message, sender, callback) => {
 
 function playAudio() {
   audio = new Audio("http://stream.yammat.fm:8000/yammat.mp3");
+
+  // If playback stops or fails, revert the icon so you don't get trolled all day.
+  audio.addEventListener("pause", () => {
+    isPlaying = false;
+    tryingToPlay = false;
+    setActionIcon("paused");
+  });
+
+  audio.addEventListener("ended", () => {
+    isPlaying = false;
+    tryingToPlay = false;
+    setActionIcon("paused");
+  });
+
+  audio.addEventListener("error", () => {
+    isPlaying = false;
+    tryingToPlay = false;
+    setActionIcon("paused");
+  });
 }
